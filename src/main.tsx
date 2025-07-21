0f2d7c946132387a7b14959b726b0918009098f8
MyFengShuiReport <myfengshuireport@gmail.com>
2025-07-21 22:20:10 +0800

    Add BrowserRouter basename and deploy script

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // 👈 Add this
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/harmony-birthdate-calculator/">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
